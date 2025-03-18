d1f3eb7d0c79017b0c5163bb9a1641dbeffc93e9
Noemi1977 <noemi.casaprima.pendas@gmail.com>
2025-03-18 19:24:03 +0100

    G9M-10 create navBar

--- comics-market/src/App.jsx
+++ comics-market/src/App.jsx
@@ -1,14 +1,24 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import NavBar from "./components/NavBar/NavBar.jsx";
 import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
-      
+      <Router>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Inicio />} />
+          {/* <Route path="/Sobrenosotros" element={<Sobrenosotros />} />
+          <Route path="/Adopta" element={<><Adopta /><CatsSlider /></>} />
+          <Route path="/Contacto" element={<AdoptForm />} />
+          <Route path="/construction" element={<PageInConstruction />} /> */}
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      </Router>
     </>
   )
 }
--- comics-market/src/components/NavBar/NavBar.jsx
+++ comics-market/src/components/NavBar/NavBar.jsx
@@ -1,8 +1,27 @@
+
+import React from "react";
 import "./NavBar.css";
 
 const NavBar = () => {
   return (
-    <nav>
+       <nav className="navbar">
+    <NavLink to="/" className="logo-container">
+      <img src={logo} alt="Logo" className="logo" />
+    </NavLink>
+    <div className="nav-links">
+      <NavLink className="nav-link" to="/" activeclassname="active">
+        Inicio
+      </NavLink>
+      <NavLink className="nav-link" to="/Adopta" activeclassname="active">
+        Adopta
+      </NavLink>
+      <NavLink className="nav-link" to="/Sobrenosotros" activeclassname="active">
+        Sobre Nosotros
+      </NavLink>
+      <NavLink className="nav-link" to="/Contacto" activeclassname="active">
+        Contacto
+      </NavLink>
+      </div>
     </nav>
   );
 };
