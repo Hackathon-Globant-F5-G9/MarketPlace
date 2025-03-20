06bbb27c41e3b6859d826a2c075cd207c15d4ee1
einartech <imalovelyghost13@gmail.com>
2025-03-20 20:34:07 +0100

    fix

--- comics-market/src/components/button/Button.jsx
+++ comics-market/src/components/button/Button.jsx
@@ -1,3 +1,13 @@
-export default function Button({ className, text }) {
-  return <button className={className}>{text}</button>;
-}
+import "./Button.module.css";
+
+const Button = ({ text, buttonClass, handleClick }) => {
+  return (
+    <>
+      <button className={buttonClass} onClick={handleClick}>
+        {text}
+      </button>
+    </>
+  );
+};
+
+export default Button;
