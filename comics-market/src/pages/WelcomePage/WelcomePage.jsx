187f7cb5b7dd97738cf257d503f3b202c3505c1b
diegodistefano <diego.distefano.1989@gmail.com>
2025-03-20 23:12:34 +0100

    style fonts responsive

--- comics-market/src/pages/WelcomePage/WelcomePage.jsx
+++ comics-market/src/pages/WelcomePage/WelcomePage.jsx
@@ -9,7 +9,7 @@ export default function WelcomePage() {
       <Header />
       <div className={styles.container}>
         <h1>BOOM COMICS</h1>
-        <h6 className={styles.blueTitle}>The Ultimate Comic Heaven!</h6>
+        <h6 className={styles.container}>The Ultimate Comic Heaven!</h6>
         <p className={styles.text}>
           WHERE LEGENDS ARE BORN! Exclusive, limited-edition comics await—rare finds, iconic stories, unstoppable passion. Enter the ultimate collector’s universe! 💥🔥
         </p>
